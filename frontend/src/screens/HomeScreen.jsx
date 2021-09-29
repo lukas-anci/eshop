e976c80f9a6bec774e89b72aeecaabf823f546f0
Lukas <lukas.ancikevicius@gmail.com>
2021-09-29 13:44:41 +0300

    proxy to frontend package.json, home screen useeffect render

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,7 +1,23 @@
 import './HomeScreen.css';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+// Components
 import Product from '../components/Product';
 
+//Actions
+import { getProducts as listProducts } from './../redux/actions/productActions';
+
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const getProducts = useSelector((state) => state.getProducts);
+  const { products, loading, error } = getProducts;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
   return (
     <div className="homescreen">
       <h2 className="homescreen__title">Latest products</h2>
