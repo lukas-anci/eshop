e51b3ee361b176c46b593f1a61fad6cf2cac314c
Lukas <lukas.ancikevicius@gmail.com>
2021-09-30 16:15:04 +0300

    cart added to side bar and navbar

--- frontend/src/components/SideDrawer.jsx
+++ frontend/src/components/SideDrawer.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import './SideDrawer.css';
+import { useSelector } from 'react-redux';
 
 const SideDrawer = ({ show, click }) => {
   const sideDrawerClass = ['sidedrawer'];
@@ -7,6 +8,14 @@ const SideDrawer = ({ show, click }) => {
   if (show) {
     sideDrawerClass.push('show');
   }
+
+  const cart = useSelector((state) => state.cart);
+
+  const { cartItems } = cart;
+
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => qty + Number(item.qty), 0);
+  };
   return (
     <div className={sideDrawerClass.join(' ')}>
       <ul className="sidedrawer__links" onClick={click}>
@@ -15,7 +24,7 @@ const SideDrawer = ({ show, click }) => {
             <i className="fas fa-shopping-cart"></i>
             <span>
               Cart
-              <span className="sidedrawer__cartbadge">0</span>
+              <span className="sidedrawer__cartbadge">{getCartCount()}</span>
             </span>
           </Link>
         </li>
